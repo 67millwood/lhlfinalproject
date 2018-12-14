7a0166715e711141bb0be88c93e4e529e9fc1d9e
Keith McSpurren <info@track2development.com>
2018-12-13 20:17:51 -0500

    minor changes to promote connectivity.  It works right now.

--- math_frontend/src/App.js
+++ math_frontend/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 class App extends Component {
   conponentDidMount() {
-    const websocket = ActionCable.createConsumer("ws://localhost:3000/websocket")
+    const websocket = ActionCable.createConsumer("ws://localhost:3006/websocket")
     window.socket = websocket.subscriptions.create({
       channel: 'WebNotificationsChannel'
     },
